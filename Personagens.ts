bf74614eb19bc7d9616dd2e7cf81452ccffca3fb
LEONARDO SOUZA BEZERRA <04308082080@senacrs.edu.br>
2025-08-14 20:43:18 -0300

    adiciona personagem

---
+++ Personagens.ts
@@ -0,0 +1,19 @@
+class personagem {
+    nome: string = "";
+    classe: string = "";
+    raca: string = "";
+    nivel: number = 0;
+    vida: number = 0; 
+    arma: string = "";
+    mana: number = 0;
+    manaAtual: number = 0;
+    manaMaxima: number = 0;
+    vidaAtual: number = 0;
+    vidaMaxima: number = 0;
+    forca: number = 0;
+    agilidade: number = 0;
+    poderAtaque: number = 0;
+
+
+
+}
