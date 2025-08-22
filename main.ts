a9ade37e91b9baf9c870c36210771be90ea5a998
LEONARDO SOUZA BEZERRA <04308082080@senacrs.edu.br>
2025-08-21 21:46:24 -0300

    adiciona menu de opções

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { personagem } from "./Personagens";
+import prompt from "prompt-sync";
+const teclado = prompt();
 
-const p: personagem = new personagem();
-p.nome = "Edécio";
+const p: personagem = new personagem("edécio");
 p.classe = "Monge";
 p.raca = "morto-vivo";
 p.nivel = Math.floor(1 + Math.random() * 99);
@@ -12,6 +13,35 @@ p.vidaMaxima = 100;
 p.vidaAtual = p.vidaMaxima;
 p.poderAtaque = 1;
 p.forca = 1;
+//menu de opções
+while (true) {
 
-console.table(p);
+    console.log("+------------------- menu------------------+");
+    console.log("|1.treinar poder de ataque|");
+    console.log("|2.ver status                 |");
+    console.log("|9.sair                   |");
+    console.log("+------------------------------+");
+    const escolha: number = +teclado("Escolha uma opção: ");
+    if (escolha === 9) {
+        break;
+    }
+    switch (escolha) {
+        case 1:
+            treinarPoderAtaque(p);
+            break;
+        case 2:
+            console.table(p);
+            break;
 
+
+
+        default:
+            console.log("opção invalida");
+    }
+function treinarPoderAtaque(p: personagem): void {
+    p.treinarPoderAtaque();
+   
+}
+} 
+//função para treinar a desgraça do personagem agora funciona
+ 
