a578658777c764468a5ff4751e3f49f8faaf7d3e
LEONARDO SOUZA BEZERRA <04308082080@senacrs.edu.br>
2025-08-21 21:44:48 -0300

    adiciona cosntrutor e metodo de treino ataque

--- Personagens.ts
+++ Personagens.ts
@@ -1,19 +1,42 @@
 export class personagem {
-    nome: string = "";
-    classe: string = "";
-    raca: string = "";
-    nivel: number = 0;
-    vida: number = 0; 
-    arma: string = "";
-    mana: number = 0;
-    manaAtual: number = 0;
-    manaMaxima: number = 0;
-    vidaAtual: number = 0;
-    vidaMaxima: number = 0;
-    forca: number = 0;
-    agilidade: number = 0;
-    poderAtaque: number = 0;
+    nome: string;
+    classe: string ;
+    raca: string ;
+    nivel: number ;
+    vida: number ;
+    arma: string ;
+    mana: number ;
+    manaAtual: number ;
+    manaMaxima: number ;
+    vidaAtual: number; 
+    vidaMaxima: number ;
+    forca: number ;
+    agilidade: number; 
+    poderAtaque: number ;
+
+    // Construtor da classe personagem usado para inicializar os atributos
+    // cria uma instância de personagem com um nome e define os atributos iniciais
+ constructor(nome: string) {
+        this.nome = nome;
+        this.classe = "";
+        this.raca = "";
+        this.nivel = 0;
+        this.vida = 0; 
+        this.arma = "";
+        this.mana = 0;
+        this.manaAtual = 0;
+        this.manaMaxima = 0;
+        this.vidaAtual = 0;
+        this.vidaMaxima = 0;
+        this.forca = 0;
+        this.agilidade = 0;
+        this.poderAtaque = 0;
+    }
+ treinarPoderAtaque(): void {
+        this.poderAtaque += 3 + this.poderAtaque * 1.1;
+    }
 
 
 
 }
+
